638e3bd8dd6dfc86c6b5f13ecdcc1915a2cdbe22
Abisso-Beretta <360841742@qq.com>
2025-01-14 10:21:35 +0800

    Update configuration for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true
-    },
-    assetPrefix: '/name-generator'
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/name-generator',
+  assetPrefix: '/name-generator/'
+}
+
+module.exports = nextConfig
